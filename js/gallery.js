828ac0debc0b37ddac48385a5587aa948eedda59
DevilHunter1337 <4595919+DevilHunter1337@users.noreply.github.com>
2026-02-02 19:07:52 -0900

    Refactor showTab function to use event parameter

--- js/gallery.js
+++ js/gallery.js
@@ -1,9 +1,13 @@
-.function showTab(tab) {
-  document.querySelectorAll('.gallery').forEach(g => g.classList.remove('active'));
-  document.querySelectorAll('.tabs button').forEach(b => b.classList.remove('active'));
+function showTab(e, tab) {
+  document.querySelectorAll('.gallery').forEach(g =>
+    g.classList.remove('active')
+  );
+  document.querySelectorAll('.tabs button').forEach(b =>
+    b.classList.remove('active')
+  );
 
   document.getElementById(tab).classList.add('active');
-  event.target.classList.add('active');
+  e.target.classList.add('active');
 }
 
 function openLightbox(src) {

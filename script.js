d387c4f9ca922c574aff333ce11a6f4a988b48a2
DevilHunter1337 <4595919+DevilHunter1337@users.noreply.github.com>
2026-02-04 18:54:35 -0900

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,16 @@ function openLightbox(src) {
 function closeLightbox() {
   document.getElementById("lightbox").style.display = "none";
 }
+
+function showTab(event, tabId) {
+  document.querySelectorAll(".tab-content").forEach(tab => {
+    tab.classList.remove("active");
+  });
+
+  document.querySelectorAll(".tab-btn").forEach(btn => {
+    btn.classList.remove("active");
+  });
+
+  document.getElementById(tabId).classList.add("active");
+  event.target.classList.add("active");
+}
